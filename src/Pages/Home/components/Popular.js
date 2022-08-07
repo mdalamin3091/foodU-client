4e8a0ad99ca6ac5b3af00d401dc45ddc4da9054f
akalamin <akalaminamin91@gmail.com>
2022-08-07 11:59:44 -0700

    popular section product slice

--- src/Pages/Home/components/Popular.js
+++ src/Pages/Home/components/Popular.js
@@ -25,7 +25,7 @@ const Popular = () => {
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:px-0">
             {filteredData
-              ?.slice(0, 6)
+              ?.slice(0, 8)
               .map((product) => (
                 <Product
                   key={product._id}
