7086cf2a0c6f268a9c85b4260ec99f25077c5eac
akalamin <akalaminamin91@gmail.com>
2022-08-06 23:54:06 -0700

    change some text

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -75,7 +75,7 @@ const Contact = () => {
                 Post-ironic portland shabby chic echo park, banjo fashion axe
               </p>
               <div className="relative mb-4">
-                <label for="email" className="leading-7 text-sm text-gray-600">
+                <label htmlFor="email" className="leading-7 text-sm text-gray-600">
                   Email
                 </label>
                 <input
@@ -86,7 +86,7 @@ const Contact = () => {
                 />
               </div>
               <div className="relative mb-4">
-                <label for="message" className="leading-7 text-sm text-gray-600">
+                <label htmlFor="message" className="leading-7 text-sm text-gray-600">
                   Message
                 </label>
                 <textarea
