b26b6460efcc0eefdd99281351ddcc3b3ddb9678
akalamin <akalaminamin91@gmail.com>
2022-08-08 14:39:17 -0700

    change loader logo

--- src/Shared/Loader/Loader.js
+++ src/Shared/Loader/Loader.js
@@ -3,9 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='flex items-center justify-center h-[90vh]'>
-        {/* <img src="https://i.ibb.co/41ZJc41/Reload-1s-200px.gif" alt="loader" /> */}
-        {/* <img src="https://i.ibb.co/Sv6dGd1/Eclipse-1s-200px.gif" alt="loader" /> */}
-        <img src="https://i.ibb.co/6RXs0xY/toptal-blog-image-1489080120310-07bfc2c0ba7cd0aee3b6ba77f101f493.gif" alt="loader" />
+        <img src="https://i.ibb.co/sQympMg/6d17b970849f62353e7509daff49db3f.gif" alt="loader" />
     </div>
   )
 }
