e8b57ae7c70dd51aa4b00fecffe28c953988a7fa
akalamin <akalaminamin91@gmail.com>
2022-08-08 14:44:16 -0700

    again change loader

--- src/Shared/Loader/Loader.js
+++ src/Shared/Loader/Loader.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='flex items-center justify-center h-[90vh]'>
-        <img src="https://i.ibb.co/sQympMg/6d17b970849f62353e7509daff49db3f.gif" alt="loader" />
+        <img src="https://i.ibb.co/6g9hsNF/2c2e8cc2269e65efbdb7fed6f5a2abf6.gif" alt="loader" />
     </div>
   )
 }
